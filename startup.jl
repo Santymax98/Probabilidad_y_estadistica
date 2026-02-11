4e9ac515c333446f7d6887559f32c157f5faafad
santymax98 <santymax9807@gmail.com>
2026-02-11 15:51:40 -0500

    modified:   startup.jl

--- startup.jl
+++ startup.jl
@@ -1,27 +1,27 @@
 # Este archivo lanzará Pluto cuando lo ejecutes en Binder
 
-using Pkg
-Pkg.activate(".")
+#using Pkg
+#Pkg.activate(".")
 
 # Asegurarse que Pluto esté instalado
-if !haskey(Pkg.project().dependencies, "Pluto")
-    @info "Instalando Pluto..."
-    Pkg.add("Pluto")
-end
+#if !haskey(Pkg.project().dependencies, "Pluto")
+#    @info "Instalando Pluto..."
+#    Pkg.add("Pluto")
+#end
 
-if !haskey(Pkg.project().dependencies, "PlutoUI")
-    @info "Instalando PlutoUI..."
-    Pkg.add("PlutoUI")
-end
+#if !haskey(Pkg.project().dependencies, "PlutoUI")
+#    @info "Instalando PlutoUI..."
+#    Pkg.add("PlutoUI")
+#end
 
-using Pluto
+#using Pluto
 
 # Iniciar Pluto con la configuración correcta para Binder
-@info "Iniciando servidor Pluto..."
-Pluto.run(
-    host="0.0.0.0",
-    port=1234,
-    launch_browser=false,
-    require_secret_for_access=false,
-    require_secret_for_open_links=false
-)
+#@info "Iniciando servidor Pluto..."
+#Pluto.run(
+#    host="0.0.0.0",
+#    port=1234,
+#    launch_browser=false,
+#    require_secret_for_access=false,
+#    require_secret_for_open_links=false
+#)
